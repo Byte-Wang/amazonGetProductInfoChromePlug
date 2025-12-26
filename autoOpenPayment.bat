211d79abe2194fdc25c6db37c80b2d42379aefc5
王志杰 <745748376@qq.com>
2025-12-26 21:56:24 +0800

    自动打开改价页面

--- autoOpenPayment.bat
+++ autoOpenPayment.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 :: ========== 配置区 ==========
 set "URL=https://sellercentral.amazon.com.au/payments/dashboard/index.html/ref=xx_payments_favb_xx"
-set "INTERVAL=60"  :: 单位：秒（600 = 10分钟）
+set "INTERVAL=60"  :: 单位：秒（86400 = 24小时）
 set "BROWSER=C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
 :: =========================
 
@@ -11,6 +11,8 @@ echo 正在启动定时任务...
 echo 每 %INTERVAL% 秒将打开: %URL%
 echo 按 Ctrl+C 可终止脚本。
 echo.
+
+timeout /t %INTERVAL% /nobreak >nul
 
 :loop
 echo [%date% %time%] 正在打开 URL...
